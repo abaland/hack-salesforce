1ee06198aea8ca7a82d0b4ab12756396c3c9d18e
adrien <adrienbaland42@gmail.com>
2021-04-07 22:33:35 +0900

    Removed unused code

--- src/chronus.go
+++ src/chronus.go
@@ -55,49 +55,7 @@ func (ch *chronus) Login() error {
 }
 
 func (ch *chronus) RegisterWork() error {
-
-	//today := time.Now()
-	//
-	//// 勤務表タブをクリック
-	//if err := sf.Page.FindByID(WorkTabID).Click(); err != nil {
-	//	return err
-	//}
-	//
-	//// ちょっと待つ
 	time.Sleep(60 * time.Second)
-	//
-	//// 月を選択
-	//err := sf.Page.FindByID(MonthListID).Select(today.Format(MonthListTextFormat))
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//// ちょっと待つ
-	//time.Sleep(ChronusSleepTime)
-	//
-	//// 勤務表の行情報イテレーション
-	//startDate := time.Date(today.Year(), today.Month()-1, 1, 0, 0, 0, 0, time.UTC)
-	//for d := startDate; d.Month() == startDate.Month(); d = d.AddDate(0, 0, 1) {
-	//
-	//	day := d.Format("2006-01-02")
-	//	workStatusSelector := "tr #" + WorkRowId + day + " td.vstatus"
-	//	workStartSelector := "tr #" + WorkRowId + day + " td.vst"
-	//	workEndSelector := "tr #" + WorkRowId + day + " td.vet"
-	//
-	//	workStatusText, _ := sf.Page.Find(workStatusSelector).Attribute("title")
-	//	dayOffBool := workStatusText == DayOffValue
-	//	startText, _ := sf.Page.Find(workStartSelector).Text()
-	//	endText, _ := sf.Page.Find(workEndSelector).Text()
-	//
-	//	workdayDetails := workday{
-	//		Day:       day,
-	//		DayOff:    dayOffBool,
-	//		StartTime: startText,
-	//		EndTime:   endText,
-	//	}
-	//
-	//	sf.WorkMonth = append(sf.WorkMonth, workdayDetails)
-	//}
 
 	return nil
 }
--- src/main.go
+++ src/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"github.com/sclevine/agouti"
+	"io/ioutil"
 )
-
-type Project struct {
-	Name     string `json:"name"`
-	Duration string `json:"duration"`
-}
 
 var (
 	configPath string
-	jsonFile   string
 )
 
 func main() {
 	flag.StringVar(&configPath, "config_path", "", "ini config path")
-	flag.StringVar(&jsonFile, "jsonfile", "", "json file for dailywork")
 	flag.Parse()
 
 	logger := NewLogger()
@@ -45,28 +40,27 @@ func main() {
 		return
 	}
 	defer driver.Stop()
-	//sf, err := driver.NewSalesForce(salesforceConfig.User, salesforceConfig.Password)
-	//if err != nil {
-	//	logger.Errorf("Failed to create instance:%v", err)
-	//	return
-	//}
-	//err = sf.Login()
-	//if err != nil {
-	//	logger.Errorf("Failed to login:%v", err)
-	//	return
-	//}
-	//
-	//// Setting Daily Works
-	//if err := sf.ParseWork(); err != nil {
-	//	logger.Errorf("Failed to RegisterWork:%v", err)
-	//	return
-	//}
-	//logger.Infof("finish to parse work.")
-	//
-	//
-	//// Write file to json TODO Add Chronus registration
-	//file, _ := json.MarshalIndent(sf.WorkMonth, "", " ")
-	//_ = ioutil.WriteFile("test.json", file, 0644)
+	sf, err := driver.NewSalesForce(salesforceConfig.User, salesforceConfig.Password)
+	if err != nil {
+		logger.Errorf("Failed to create instance:%v", err)
+		return
+	}
+	err = sf.Login()
+	if err != nil {
+		logger.Errorf("Failed to login:%v", err)
+		return
+	}
+
+	// Setting Daily Works
+	if err := sf.ParseWork(); err != nil {
+		logger.Errorf("Failed to RegisterWork:%v", err)
+		return
+	}
+	logger.Infof("finish to parse work.")
+
+	// Write file to json TODO Add Chronus registration
+	file, _ := json.MarshalIndent(sf.WorkMonth, "", " ")
+	_ = ioutil.WriteFile("test.json", file, 0644)
 
 	ch, err := driver.NewChronus(chronusConfig.User, chronusConfig.Password)
 	if err != nil {
