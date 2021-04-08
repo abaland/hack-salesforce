ddd663fce2d7180b5a13260c0125ba6bdde45736
adrien <adrienbaland42@gmail.com>
2021-04-08 22:42:08 +0900

    Added class for dealing with dayschedule

---
+++ src/dayschedule.go
@@ -0,0 +1,32 @@
+package main
+
+import "time"
+
+type DayScheduleStr struct {
+	WorkStart string
+	WorkEnd   string
+
+	Break1Start string
+	Break1End   string
+
+	Break2Start string
+	Break2End   string
+}
+
+type DaySchedule struct {
+	WorkStart time.Time
+	WorkEnd   time.Time
+
+	Break1Start time.Time
+	Break1End   time.Time
+
+	Break2Start time.Time
+	Break2End   time.Time
+}
+
+func (ds *DaySchedule) IsRegularBreak() bool {
+
+	break1Time := ds.Break1End.Sub(ds.Break1Start).Minutes()
+	break2Time := ds.Break2End.Sub(ds.Break2Start).Minutes()
+	return break1Time+break2Time == 60
+}
